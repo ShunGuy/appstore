21b77daaf05146132add29743e3a5d2714b8cd90
ShunGuy <alantozero33@gmail.com>
2017-02-07 21:47:01 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'app#index'
+
+
+  resources :items, only: [:new, :edit]
+
+  namespace :api do
+    namespace :v1 do
+      resources :items, only: [:index, :create, :destroy, :update], defaults: { format: 'json' }
+    end
+  end
 end
